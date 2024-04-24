ccb4ad35d83d36b499a1b64d2a4223c167ca0e06
Miguel <mibaca27@gmail.com>
2024-04-24 19:04:58 +0200

    Mejorado imagenes y tamaño

---
+++ docs/chunk-L56ASSSE.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-37E4W4H6.js";import"./chunk-JJSDBTMF.js";import"./chunk-LNHBMWTA.js";import"./chunk-7B4WEPGY.js";import"./chunk-VK4RGVHF.js";import"./chunk-ROIV5QGY.js";import"./chunk-VZFTKLKS.js";export{a as LoginComponent};
---
+++ docs/chunk-VK4RGVHF.js
@@ -0,0 +1,1 @@
+var e={baseUrl:"http://localhost:3000",siteKey:"6LdRA70pAAAAAABQWbOV1zdi3EooM_emXNQOiFvC"};export{e as a};
