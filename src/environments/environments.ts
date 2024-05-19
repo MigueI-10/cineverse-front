90322221038e4ae8eb4554fa050c8f6f93d923f7
Miguel <mibaca27@gmail.com>
2024-05-19 15:25:38 +0200

    Cambio a prod url

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,7 +1,7 @@
 
 export const environment ={
-    // baseUrl: 'https://cineverse-back-production.up.railway.app',
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'https://cineverse-back-production.up.railway.app',
+    // baseUrl: 'http://localhost:3000',
     siteKey: '6LdRA70pAAAAAABQWbOV1zdi3EooM_emXNQOiFvC'
 }
 
