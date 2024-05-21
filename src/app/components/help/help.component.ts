cc7671633549a5e5079c077c87b41a20fe5d7fc3
Miguel <mibaca27@gmail.com>
2024-05-21 19:45:31 +0200

    Continuacion desarrollo. Faltaba mini help y video yt

--- src/app/components/help/help.component.ts
+++ src/app/components/help/help.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-help',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './help.component.html',
   styleUrl: './help.component.css'
 })
 export class HelpComponent {
 
+  safeURL :SafeResourceUrl
+  videoURL = "https://www.youtube.com/watch?v=R6Mn_yGAzjE"
+
+  constructor( private _sanitizer: DomSanitizer){
+    this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl(this.videoURL);
+ }
 }
--- src/app/guards/isNotAuthenticated.guard.ts
+++ src/app/guards/isNotAuthenticated.guard.ts
@@ -2,6 +2,7 @@ import { inject } from "@angular/core";
 import { CanActivateFn, Router } from "@angular/router";
 import { AuthStatus } from "../interfaces";
 import { AuthService } from "../services/auth.service";
+import { map } from "rxjs";
 
 export const isNotAuthenticatedGuard: CanActivateFn = (route, state) => {
 
@@ -10,12 +11,28 @@ export const isNotAuthenticatedGuard: CanActivateFn = (route, state) => {
 
     
   //authService.authStatus()
-    if(authService.authStatus() === AuthStatus.authenticated){
-      router.navigateByUrl('/home')
-      return false;
-    }
+  // console.log(authService.authStatus());
+  //   if(authService.authStatus() === AuthStatus.authenticated){
+  //     router.navigateByUrl('/home')
+  //     return false;
+  //   }
   
-    return true;
+  //   return true;
+
+  return authService.checkAuthStatus().pipe(
+    map(isAuthenticated => {
+      // console.log(isAuthenticated);
+      if (isAuthenticated) {
+        // Si el usuario está autenticado y es un administrador, permitir el acceso
+        router.navigateByUrl('/home');
+        return false
+      } else {
+        // Si el usuario no está autenticado, redirigirlo a la página de inicio de sesión
+        
+        return true;
+      }
+    })
+  );
   
   };
   
