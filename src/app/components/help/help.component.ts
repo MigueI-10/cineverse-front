04ac776c4d206cc83131b4ba7524f3772ad3a4b9
Miguel <mibaca27@gmail.com>
2024-05-28 19:37:00 +0200

    Añadidos los videos a help

--- src/app/components/help/help.component.ts
+++ src/app/components/help/help.component.ts
@@ -1,20 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { TranslateModule } from '@ngx-translate/core';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-help',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './help.component.html',
   styleUrl: './help.component.css'
 })
-export class HelpComponent {
+export class HelpComponent implements OnInit {
 
-  safeURL :SafeResourceUrl
+  safeURL: SafeResourceUrl
   videoURL = "https://www.youtube.com/watch?v=R6Mn_yGAzjE"
 
-  constructor( private _sanitizer: DomSanitizer){
+  public isLoggedIn: boolean = false;
+  public mainRole: string = ""
+
+  //video normal: https://www.youtube.com/watch?v=4w_mLk8jvGI
+
+  constructor(private _sanitizer: DomSanitizer,
+    private authService: AuthService,
+  ) {
     this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl(this.videoURL);
- }
+  }
+
+  ngOnInit(): void {
+    this.authService.checkAuthStatus().subscribe(
+      res => {
+        this.isLoggedIn = res
+        if (this.isLoggedIn) {
+          const { ...user } = this.authService.usrActual();
+
+        
+          this.mainRole = user.roles[0]
+          
+        }
+
+      }
+    )
+  }
 }
