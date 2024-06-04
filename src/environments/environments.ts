e65bca5a5ad0547f2103ad94ded734cf5745b764
Miguel <mibaca27@gmail.com>
2024-06-04 15:55:33 +0200

    Añadido a render el deploy de la url

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,7 @@
 
 export const environment ={
-    baseUrl: 'https://cineverse-back-production.up.railway.app',
+    // baseUrl: 'https://cineverse-back-production.up.railway.app',
+    baseUrl: 'https://cineverse-back-kfuc.onrender.com',
     // baseUrl: 'http://localhost:3000',
     siteKey: '6LdRA70pAAAAAABQWbOV1zdi3EooM_emXNQOiFvC'
 }
